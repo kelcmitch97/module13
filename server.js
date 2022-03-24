4f7318bb85a959cb0e8290a082285d15e36b5b28
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-03-24 20:04:34 -0230

    Add handelbars

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
 const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
